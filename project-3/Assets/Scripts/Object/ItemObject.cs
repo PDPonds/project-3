dce064eeb3b7edc3f2ee0d611c43a2f23d95d661
Po2s <pondandmeen@gmail.com>
2024-12-31 16:53:09 +0700

    item object

--- project-3/Assets/Scripts/Object/ItemObject.cs
+++ project-3/Assets/Scripts/Object/ItemObject.cs
@@ -2,12 +2,13 @@ using UnityEngine;
 
 public class ItemObject : MonoBehaviour, IActionObject
 {
-    ItemSlot slot;
+    [SerializeField] ItemSlot slot;
 
     public void Setup(ItemSO item, int count)
     {
         slot.item = item;
         slot.count = count;
+        InitVisual();
     }
 
     public void Setup(ItemSO item, float maxValue, float curValue)
@@ -16,11 +17,22 @@ public class ItemObject : MonoBehaviour, IActionObject
         slot.count = 1;
         slot.maxValue = maxValue;
         slot.curValue = curValue;
+        InitVisual();
+    }
+
+    void InitVisual()
+    {
+        if (slot.item != null)
+        {
+            GameObject visual = Instantiate(slot.item.itemPrefab, transform);
+        }
     }
 
     public void Action()
     {
+        UIManager.Instance.CloseInteractiveChoice();
         GameManager.Instance.playerInventory.AddItem(slot);
+        Destroy(gameObject);
     }
 
     public string ActionName()
